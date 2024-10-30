b925e658d3fa4830463fcb1f0de3f1d600d8f03c
Marc Fernández <marcausente@gmail.com>
2024-10-30 19:55:57 +0100

    Maquetado

--- FindNumber/ContentView.swift
+++ FindNumber/ContentView.swift
@@ -15,18 +15,33 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             
-            Text("Elige bien!!!")
+            Text("81")
                 .padding(.vertical)
-            HStack{
+                .fontWeight(.bold)
+                .font(.system(size: 40))
+        
+            HStack{ //Una parte horizontal dentro de la vertical para hacer la linea con los numeros delante y detrás
                 Text("1")
                     .padding(.leading)
+                    .fontWeight(.bold)
                 Slider(value: .constant(50))
                     .padding(.horizontal)
                 Text("100")
+                    .fontWeight(.bold)
                     .padding(.trailing)
             }
             .padding(.top)
-    
+            
+            Spacer().frame(height: 50) // Espacio vacio
+            
+            Button("Elige!") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.automatic/*@END_MENU_TOKEN@*/)
+            .padding(.all)
+            .background(Color.blue)
+            .foregroundColor(Color.white)
+            .cornerRadius(100) //Esto te redondea los bordes
 
         }
     }
